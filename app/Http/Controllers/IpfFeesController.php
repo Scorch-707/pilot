8000dce9963f43b53c0a1374bbec119e70e1997a
Scorch-707 <31009669+Scorch-707@users.noreply.github.com>
2017-08-21 02:54:38 -0700

    Update IpfFeesController.php

--- app/Http/Controllers/IpfFeesController.php
+++ app/Http/Controllers/IpfFeesController.php
@@ -17,10 +17,39 @@ class IpfFeesController extends Controller
     }
 
   
-    public function store(StoreIPFFee $request)
+    public function store(Request $request)
     {
-        $ipf = IpfFee::create($request->all());
-        return $ipf;
+       $ipf_header = new ImportProcessingFeeHeader;
+        $ipf_header->dateEffective = $request->dateEffective;
+        $ipf_header->save();
+
+        $_minimum = json_decode(stripslashes($request->minimum), true);
+        $_maximum = json_decode(stripslashes($request->maximum), true);
+        $_amount = json_decode(stripslashes($request->amount), true);
+
+
+        $tblRowLength = $request->tblLength;
+
+        for($x = 0; $x < $tblRowLength; $x++)
+        {
+          $ipf_detail = new ImportProcessingFeeDetail;
+          $ipf_detail->ipf_headers_id = $ipf_header->id;
+          $ipf_detail->minimum = (string)$_minimum[$x];
+          $ipf_detail->maximum = (string)$_maximum[$x];
+          $ipf_detail->amount = (string)$_amount[$x];
+          $ipf_detail->save();
+        }
+      }
+
+      public function update(StoreIPFFee $request, $id)
+      {
+          $ipf_fee = IpfFee::findOrFail($id);
+        $ipf_fee->minimum = $request->minimum;
+        $ipf_fee->maximum = $request->maximum;
+        $ipf_fee->amount = $request->amount;
+        $ipf_fee->save();
+
+        return $ipf_fee;
     }
 
 
